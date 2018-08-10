cfb59b2dd16dcb0ffde41efd50860650359ae950
chrisli4 <chrisli41@gmail.com>
2018-08-10 01:21:57 -0700

    production build

--- server/build/server.js
+++ server/build/server.js
@@ -1,15 +1,29 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-const app = express();
+'use strict';
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const router = express.Router();
-router.get('/cities', (req, res) => {
-  const cities = [{ name: 'New York City', population: 8175133 }, { name: 'Los Angeles', population: 3792621 }, { name: 'Chicago', population: 2695598 }];
+
+var router = express.Router();
+
+var staticFiles = express.static(path.join(__dirname, '../../client/build'));
+app.use(staticFiles);
+
+router.get('/cities', function (req, res) {
+  var cities = [{ name: 'New York City', population: 8175133 }, { name: 'Los Angeles', population: 3792621 }, { name: 'Chicago', population: 2695598 }];
   res.json(cities);
 });
+
 app.use(router);
+
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles);
+
 app.set('port', process.env.PORT || 3001);
-app.listen(app.get('port'), () => {
-  console.log(`Listening on ${app.get('port')}`);
+app.listen(app.get('port'), function () {
+  console.log('Listening on ' + app.get('port'));
 });
